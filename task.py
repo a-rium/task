292cad1871ab71d596ff846d929d002e9f92426b
Rium <kinglessqueenrium@gmail.com>
2024-06-09 19:55:23 +0200

    added `context add <context>`

--- task.py
+++ task.py
@@ -1,4 +1,60 @@
+import argparse
+import sys
+import os
+import os.path
+
+
+ROOT = os.path.expanduser(os.path.join('~', '.task'))
+
+
+def wrapped_os_mkdir(dirpath: str) -> bool:
+    created = False
+    try:
+        os.mkdir(dirpath)
+        created = True
+    except (FileExistsError, FileNotFoundError):
+        pass
+    return created
+
+
+def mkdir(dirpath: str, *, recursive=False) -> bool:
+    created = False
+    if recursive:
+        dirtree, directory = os.path.split(dirpath)
+        if dirtree != os.path.sep:
+            mkdir(dirtree, recursive=recursive)
+        created = wrapped_os_mkdir(dirpath)
+    else:
+        created = wrapped_os_mkdir(dirpath)
+    return created
+
+
+def create_context(name: str) -> bool:
+    return mkdir(os.path.join(ROOT, 'context', name), recursive=True)
+
+
 def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode')
+    parser.add_argument('ignore', nargs='*')
+
+    args = parser.parse_args(sys.argv[1:])
+    if args.mode == 'context':
+        parser = argparse.ArgumentParser()
+        parser.add_argument('mode')
+        parser.add_argument('ignore', nargs='*')
+
+        args = parser.parse_args(sys.argv[2:])
+
+        if args.mode == 'add':
+            parser = argparse.ArgumentParser()
+            parser.add_argument('context_name')
+            parser.add_argument('ignore', nargs='*')
+
+            args = parser.parse_args(sys.argv[3:])
+            create_context(args.context_name)
+
+
     return 0
 
 
